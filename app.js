a984855116d27fab42b468884f50b7266de73682
vermakusum593 <vermakusum593@gmail.com>
2025-02-28 21:21:05 +0100

    added the app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('Start of the program');
+
+function delayedMessage() {
+    console.log('This message appears after 3 seconds');
+}
+
+setTimeout(delayedMessage, 3000);
+
+console.log('End of the program');
